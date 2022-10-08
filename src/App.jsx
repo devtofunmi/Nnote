86e7984403a9dbbc23d47d063f6117d1128601af
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-09 00:57:06 +0100

    add box to overide chakra default theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import LandingPage from "./pages/LandingPage";
 import DashBoard from "./pages/DashBoard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-        </Routes>
-      </Router>
+      <Box w={"100%"} minH={"100vh"} backgroundColor="white" color={"black"}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+          </Routes>
+        </Router>
+      </Box>
     </>
   );
 }
