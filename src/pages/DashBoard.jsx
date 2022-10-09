922500023d4f634e669976933239c1bfab399985
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-09 02:15:10 +0100

    add the sidebar,searchinput to the dashboard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,7 +1,30 @@
+import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import React from "react";
-
+import SideBar from "../components/SideBar";
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  return (
+    <>
+      <Flex justifyContent={"space-between"} p={"30px"} alignItems={"center"}>
+        <SideBar />
+        <Text
+          fontSize={["20px", "50px"]}
+          w={"extra-bold"}
+          fontFamily={"sans-serif"}
+        >
+          My Notes
+        </Text>
+        <Box>
+          <Input
+            variant="outline"
+            placeholder="search"
+            _placeholder={{ opacity: 1, color: "black" }}
+            bgColor={"gray.100"}
+            color={"black"}
+          />
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default DashBoard;
