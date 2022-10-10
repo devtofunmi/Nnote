592cc7e27f05b58b1598ee252edf4220348a4b09
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-10 19:16:03 +0100

    ...

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,29 +1,53 @@
 import { Box, Flex, Input, Text } from "@chakra-ui/react";
 import React from "react";
+import Main from "../components/Main";
 import SideBar from "../components/SideBar";
+import DashboardLayout from "../layout/DashboardLayout";
+
 const DashBoard = () => {
   return (
-    <>
-      <Flex justifyContent={"space-between"} p={"30px"} alignItems={"center"}>
-        <SideBar />
-        <Text
-          fontSize={["20px", "50px"]}
-          w={"extra-bold"}
-          fontFamily={"sans-serif"}
-        >
-          My Notes
-        </Text>
-        <Box>
-          <Input
-            variant="outline"
-            placeholder="search"
-            _placeholder={{ opacity: 1, color: "black" }}
-            bgColor={"gray.100"}
-            color={"black"}
-          />
-        </Box>
+    // <Flex>
+    //   <Box>
+    //     <SideBar />
+    //   </Box>
+    //   <Flex
+    //     // justifyContent={"center"}
+    //     direction={"column"}
+    //     // alignItems={"center"}
+    //   >
+    //     <Flex
+    //       pt={"20px"}
+    //       justifyContent={"space-between"}
+    //       // w={"80%"}
+    //       bg={"red.300"}
+    //     >
+    //       <Text
+    //         fontSize={["20px", "30px"]}
+    //         w={"extra-bold"}
+    //         fontFamily={"sans-serif"}
+    //       >
+    //         Nnote
+    //       </Text>
+    //       <Box>
+    //         <Input
+    //           variant="outline"
+    //           placeholder="search"
+    //           _placeholder={{ opacity: 1, color: "black" }}
+    //           bgColor={"gray.100"}
+    //           color={"black"}
+    //         />
+    //       </Box>
+    //     </Flex>
+
+    //     <Main />
+    //   </Flex>
+    // </Flex>
+    <DashboardLayout>
+      <Flex>
+        {/* <Text>Main</Text> */}
+        <Main />
       </Flex>
-    </>
+    </DashboardLayout>
   );
 };
 
