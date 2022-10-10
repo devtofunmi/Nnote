cb28a360d813e20f83a27e8c5646d376fcc9cddc
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-10 19:14:19 +0100

    sidebar ui ...

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,37 +1,29 @@
+import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
-import { BsMenuApp } from "react-icons/bs";
-import {
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import AddNote from "./AddNote";
 
 const SideBar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
-    <>
-      <Button bg={"gray.100"} onClick={onOpen}>
-        <BsMenuApp color="black" />
-      </Button>
-      <Drawer onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader borderBottomWidth="1px" bg={"black"} color={"white"}>
-            Nnote
-          </DrawerHeader>
-          <DrawerBody bgColor={"white"} color={"black"}>
-            <p>Add Note</p>
-            <p>Favourite Note</p>
-            <p>Calender</p>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
-    </>
+    <Flex
+      flexDirection={"column"}
+      bg={"purple.400"}
+      w={"100%"}
+      minH={"100vh"}
+      px={4}
+    >
+      <Text>Nnote</Text>
+      <AddNote />
+      <Link to={"/dashboard"}>
+        <Text my={2}>Overview</Text>
+      </Link>
+      <Link to={"/dashboard/favorite"}>
+        <Text my={2}>Favourite</Text>
+      </Link>
+      <Link to={"/"}>
+        <Text my={2}>Calender</Text>
+      </Link>
+    </Flex>
   );
 };
 
