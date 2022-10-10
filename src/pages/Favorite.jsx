ff4ad69f5e50082a1ef83c9b0cc7cf372266d931
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-10 19:17:07 +0100

    ...

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,15 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import DashboardLayout from "../layout/DashboardLayout";
+
+const Favorite = () => {
+  return (
+    <DashboardLayout>
+      <Flex>
+        <Text>Fav</Text>
+      </Flex>
+    </DashboardLayout>
+  );
+};
+
+export default Favorite;
