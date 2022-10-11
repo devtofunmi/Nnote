b4a54c9aef92f7cad395e7dc5367dd7a2d3fb1ee
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-11 13:28:06 +0100

    change the background and font size

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -3,11 +3,11 @@ import { MdBookmarkAdd } from "react-icons/md";
 
 const NoteCard = () => {
   return (
-    <Box width={"25%"} bg={"purple.400"} p={"17px"} borderRadius={"10px"}>
+    <Box width={"25%"} bg={"#181819"} p={"17px"} borderRadius={"10px"}>
       <Flex>
         <Box>
-          <Text fontSize={"25px"}>why im still single</Text>
-          <Text mt={5}>
+          <Text fontSize={"20px"}>why im still single</Text>
+          <Text mt={5} fontSize={"15px"}>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
             dolore, eum facere ut et repellat quibusdam laboriosam culpa vel
             quas.
