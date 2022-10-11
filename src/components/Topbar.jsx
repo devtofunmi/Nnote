7a79092d6a01e08e5266c4867f9546f8d9a9fefe
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-11 13:29:12 +0100

    add the new note component and change the backgound

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,24 +1,29 @@
-import { Avatar, Flex, Input, Text } from "@chakra-ui/react";
+import { Avatar, Box, Flex, Input, Text } from "@chakra-ui/react";
 import React from "react";
+import AddNewNote from "./AddNewNote";
 
 const Topbar = () => {
   return (
-    <Flex justify={"space-between"} px={3} py={2} bg={"purple.100"}>
+    <Flex justify={"space-between"} px={3} py={2} bg={"#181819"}>
       <Input
         w={"50%"}
-        bg={"#fff"}
+        bg={"#202225"}
         placeholder={"Search..."}
         _placeholder={{
-          color: "#000",
+          color: "#afb1b3",
         }}
       />
+      <Box>
+        <AddNewNote />
+      </Box>
       <Flex
         align={"center"}
         gap={3}
-        bg={"purple.200"}
+        bg={"#202225"}
         px={3}
         borderRadius={"md"}
         cursor={"pointer"}
+        color={"#afb1b3"}
       >
         <Text>Tofunmi</Text>
         <Avatar size={"sm"} />
