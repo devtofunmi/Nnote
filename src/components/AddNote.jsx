a8925ea9486a13bafc765a11dac9f24c149f717c
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-12 13:30:41 +0100

    style the popover

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -22,41 +22,14 @@ import {
   ModalFooter,
   Flex,
   Heading,
+  CloseButton,
 } from "@chakra-ui/react";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { MdOutlineClose, MdOutlineEditNote } from "react-icons/md";
-const AddNote = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
+const AddNote = ({ isOpen, closePopup }) => {
   return (
     <>
-      <Button
-        onClick={onOpen}
-        rightIcon={<MdOutlineEditNote />}
-        bg={"#fff"}
-        color={"purple.400 "}
-        my={5}
-      >
-        Add New Note
-      </Button>
-      {/* <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add New Note</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <Input placeholder="Title" />
-            <Textarea mt={"15px"} placeholder="Enter note" />
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
-              Save
-            </Button>
-            <Button onClick={onClose}>Cancel</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal> */}
-
       <Box
         position={"fixed"}
         w={"100%"}
@@ -64,41 +37,36 @@ const AddNote = () => {
         top={"0px"}
         left={"0px"}
         backdropBlur={"lg"}
-        display={"flex"}
+        display={isOpen ? "flex" : "none"}
         justifyContent={"center"}
         placeItems={"center"}
-        background={"   rgba(0, 0, 0, 0.5)"}
-        visibility={"hidden"}
-        zIndex={-10}
+        background={"   rgba(0, 0, 0, 0.6)"}
+        zIndex={10}
+        backdropFilter={"blur(5px)"}
       >
         <Flex
           direction={"column"}
-          background={"teal"}
+          background={"#181919"}
           w={"90vw"}
-          h={"50vh"}
           textAlign={"center"}
           justifyContent={"center"}
           placeItems={"center"}
           position={"relative"}
-          maxW={"620px"}
+          maxW={"450px"}
           padding={"30px"}
           borderRadius={"10px"}
         >
-          <Flex>
-            <Text>Add New Button</Text>
-            <Box
-              position={"absolute"}
-              top={"1rem"}
-              right={"1rem"}
-              fontSize={"2rem"}
-              cursor={"pointer"}
-            >
+          <Flex align={"center"} justify={"space-between"} w={"100%"}>
+            <Text fontSize={"1.5rem"}>Add New Note</Text>
+            <Button size={"sm"} cursor={"pointer"} onClick={() => closePopup()}>
               <MdOutlineClose />
-            </Box>
+            </Button>
           </Flex>
-          <Input placeholder="Title" />
-          <Textarea mt={"15px"} placeholder="Enter note" />
-          <Button>Submit</Button>
+          <Box w={"400px"} mt={"50px"}>
+            <Input placeholder="Title" />
+            <Textarea mt={"15px"} placeholder="Enter note" h={"150px"} />
+            <Button mt={"20px"}>Submit</Button>
+          </Box>
         </Flex>
       </Box>
     </>
