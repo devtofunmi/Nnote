1f6ad1f5e4c10a27c55852d1ed3caec20afff6e6
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-13 00:13:00 +0100

    import  images from assets

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -1,25 +1,30 @@
 import { BsCalendar } from "react-icons/bs";
 import { AiFillFileUnknown, AiOutlineFileSearch } from "react-icons/ai";
 import { MdOutlinePhonelink } from "react-icons/md";
+import { Img } from "@chakra-ui/react";
+import one from "../assets/one.jpg";
+import two from "../assets/two.jpg";
+import three from "../assets/three.jpg";
+import four from "../assets/four.jpg";
 
 const FeaturesCard = [
   {
-    icon: <BsCalendar />,
+    icon: <Img src={one} />,
     heading: "Plan and manage your day",
     text: "Start your morning with a clean slate",
   },
   {
-    icon: <AiFillFileUnknown />,
+    icon: <Img src={two} />,
     heading: "Never forget a thing",
     text: "Quickly add your tasks,notes,expenses",
   },
   {
-    icon: <AiOutlineFileSearch />,
+    icon: <Img src={three} />,
     heading: "Find everything fast",
     text: "Quickly search through your items with one tap",
   },
   {
-    icon: <MdOutlinePhonelink />,
+    icon: <Img src={four} />,
     heading: "With you, everywhere",
     text: "Accessible across all your device",
   },
