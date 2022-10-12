3c0019f89cd6d8bf33599d42b7826f1f5c5d1d14
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-12 20:09:01 +0100

    ...

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,6 @@
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
-import AddNote from "./AddNote";
 
 const SideBar = () => {
   return (
@@ -14,15 +13,15 @@ const SideBar = () => {
       pt={"30px"}
     >
       <Text fontSize={"30px"}>Nnote</Text>
-      {/* <AddNote /> */}
+
       <Link to={"/dashboard"} mt={"20px"}>
         <Text my={2}>Overview</Text>
       </Link>
+      <Link to={"/task"} mt={"20px"}>
+        <Text my={2}>Task</Text>
+      </Link>
       <Link to={"/dashboard/favorite"}>
         <Text my={2}>Favourite</Text>
-      </Link>
-      <Link to={"/"}>
-        <Text my={2}>Calender</Text>
       </Link>
     </Flex>
   );
