ce905002fb3c44fc62a7c688c71858c7160ad6ef
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-12 22:33:39 +0100

    making it responsive

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -3,18 +3,18 @@ import { MdBookmarkAdd } from "react-icons/md";
 
 const NoteCard = () => {
   return (
-    <Box width={"25%"} bg={"#181819"} p={"17px"} borderRadius={"10px"}>
+    <Box width={["90%", "25%"]} bg={"#181819"} p={"17px"} borderRadius={"10px"}>
       <Flex>
         <Box>
-          <Text fontSize={"20px"}>why im still single</Text>
-          <Text mt={5} fontSize={"15px"}>
+          <Text fontSize={["15px", "20px"]}>why im still single</Text>
+          <Text mt={5} fontSize={["10px", "15px"]}>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi
             dolore, eum facere ut et repellat quibusdam laboriosam culpa vel
             quas.
           </Text>
         </Box>
         <Button>
-          <MdBookmarkAdd fontSize={"80px"} color={"black"} />
+          <MdBookmarkAdd fontSize={["50px", "80px"]} color={"black"} />
         </Button>
       </Flex>
     </Box>
