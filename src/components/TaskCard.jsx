5870aab05b8fc06a67c821a0a74e002c67e8a2e4
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-12 20:36:29 +0100

    add checkbox and text

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,7 +1,23 @@
+import { Box, Checkbox, Flex, Text } from "@chakra-ui/react";
 import React from "react";
 
 const TaskCard = () => {
-  return <div></div>;
+  return (
+    <>
+      <Box>
+        <Flex
+          gap={"10px"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          textAlign={"center"}
+          cursor={"pointer"}
+        >
+          <Checkbox />
+          <Text>Create the term sheet</Text>
+        </Flex>
+      </Box>
+    </>
+  );
 };
 
 export default TaskCard;
