99c8bd3ddb13ee22a263a74f860a6b44acdac5de
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-13 09:08:09 +0100

    chage icon name to image

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -6,22 +6,22 @@ import four from "../assets/four.png";
 
 const FeaturesCard = [
   {
-    icon: <Img src={one} />,
+    image: <Img src={one} />,
     heading: "Plan and manage your day",
     text: "Start your morning with a clean slate, Plan ahead your day with a task in  our note",
   },
   {
-    icon: <Img src={two} />,
+    image: <Img src={two} />,
     heading: "Never forget a thing",
     text: "Quickly add your tasks,notes,expenses",
   },
   {
-    icon: <Img src={three} />,
+    image: <Img src={three} />,
     heading: "Find everything fast",
     text: "Quickly search through your items with one tap",
   },
   {
-    icon: <Img src={four} />,
+    image: <Img src={four} />,
     heading: "With you, everywhere",
     text: "Accessible across all your device",
   },
