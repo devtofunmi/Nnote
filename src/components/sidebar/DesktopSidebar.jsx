b0c0cc8917ddf8f65aded9085d984a68dd44c659
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-13 12:34:36 +0100

    create a components for the desktop sidebar

---
+++ src/components/sidebar/DesktopSidebar.jsx
@@ -0,0 +1,30 @@
+import { Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DesktopSidebar = () => {
+  return (
+    <Flex
+      flexDirection={"column"}
+      bg={"#181819"}
+      w={"100%"}
+      minH={"100vh"}
+      px={4}
+      pt={"30px"}
+    >
+      <Text fontSize={"30px"}>Nnote</Text>
+
+      <Link to={"/dashboard"} mt={"20px"}>
+        <Text my={2}>Overview</Text>
+      </Link>
+      <Link to={"/task"} mt={"20px"}>
+        <Text my={2}>Task</Text>
+      </Link>
+      <Link to={"/dashboard/favorite"}>
+        <Text my={2}>Favourite</Text>
+      </Link>
+    </Flex>
+  );
+};
+
+export default DesktopSidebar;
