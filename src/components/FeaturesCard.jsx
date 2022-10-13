e921fb07c8463f739ca7efcaf9e769187f1d19d6
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-13 10:17:46 +0100

    add isverted

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -9,21 +9,25 @@ const FeaturesCard = [
     image: <Img src={one} />,
     heading: "Plan and manage your day",
     text: "Start your morning with a clean slate, Plan ahead your day with a task in  our note",
+    isInverted: false,
   },
   {
     image: <Img src={two} />,
     heading: "Never forget a thing",
     text: "Quickly add your tasks,notes,expenses",
+    isInverted: true,
   },
   {
     image: <Img src={three} />,
     heading: "Find everything fast",
     text: "Quickly search through your items with one tap",
+    isInverted: false,
   },
   {
     image: <Img src={four} />,
     heading: "With you, everywhere",
     text: "Accessible across all your device",
+    isInverted: true,
   },
 ];
 
