47d5e845e2c53fd5bda556545f64e3fb40acce44
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-13 10:39:47 +0100

    add the sidebar for the mobile version

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,22 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
-import React, { useEffect } from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useWindowDimensions from "../hooks/useWindowsDimensions";
 
 const SideBar = () => {
   const { width } = useWindowDimensions();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [placement, setPlacement] = useState("left");
 
   useEffect(() => {
     console.log(width);
@@ -34,7 +46,36 @@ const SideBar = () => {
         </Flex>
       ) : (
         <>
-          <Button>Op</Button>
+          <Button colorScheme="blue" onClick={onOpen}>
+            Open
+          </Button>
+          <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
+            <DrawerOverlay />
+            <DrawerContent>
+              <DrawerBody>
+                <Flex
+                  flexDirection={"column"}
+                  bg={"#181819"}
+                  w={"100%"}
+                  minH={"100vh"}
+                  px={4}
+                  pt={"30px"}
+                >
+                  <Text fontSize={"30px"}>Nnote</Text>
+
+                  <Link to={"/dashboard"} mt={"20px"}>
+                    <Text my={2}>Overview</Text>
+                  </Link>
+                  <Link to={"/task"} mt={"20px"}>
+                    <Text my={2}>Task</Text>
+                  </Link>
+                  <Link to={"/dashboard/favorite"}>
+                    <Text my={2}>Favourite</Text>
+                  </Link>
+                </Flex>
+              </DrawerBody>
+            </DrawerContent>
+          </Drawer>
         </>
       )}
     </>
