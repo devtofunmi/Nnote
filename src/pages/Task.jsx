2f0ea66de5aec96ca82dbff162e9eb405335c111
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-14 17:52:24 +0100

    ....

--- src/pages/Task.jsx
+++ src/pages/Task.jsx
@@ -1,42 +1,86 @@
 import { Box, Flex, Text, Checkbox } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { MdSettingsBackupRestore } from "react-icons/md";
 // import TaskCard from "../components/TaskCard";
 import DashboardLayout from "../layout/DashboardLayout";
 
 const Task = () => {
-  const [done, setDone] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "i wanna code",
+      complete: false,
+    },
+    {
+      id: 2,
+      title: "i wanna watch movies",
+      complete: false,
+    },
+    {
+      id: 3,
+      title: "i wanna chat with the she",
+      complete: false,
+    },
+    {
+      id: 4,
+      title: "i wanna eat",
+      complete: false,
+    },
+  ]);
+
+  function complete(id) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          complete: !task.complete,
+        };
+      }
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
 
   return (
     <>
       <DashboardLayout>
         <Flex>
-          <Flex
-            justifyContent={"center"}
-            alignItems={"center"}
-            direction={"column"}
-          >
+          <Flex direction={"column"}>
             <Box>
-              <Text>Task</Text>
-
-              <Flex
-                gap={"10px"}
-                alignItems={"center"}
-                justifyContent={"center"}
-                textAlign={"center"}
-                cursor={"pointer"}
-              >
-                <Checkbox
-                  onClick={(e) => {
-                    setDone(e.target.value);
-                  }}
-                />
-                <Text>Create the term sheet</Text>
-              </Flex>
-
-              <Box>{done(console.log("done"))}</Box>
+              <Text>Tasks</Text>
+              {tasks
+                .filter((task) => !task.complete)
+                .map((t) => (
+                  <Box key={t.id}>
+                    <Flex>
+                      <Checkbox
+                        onChange={() => {
+                          complete(t.id);
+                        }}
+                      />
+                      <Text>{t.title}</Text>
+                    </Flex>
+                  </Box>
+                ))}
             </Box>
-            <Box mt={"50px"}>
-              <Text>Complete</Text>
+
+            <Box mt={"20px"}>
+              <Text>Completed</Text>
+
+              {tasks
+                .filter((task) => task.complete)
+                .map((t) => (
+                  <Flex key={t.id}>
+                    <Checkbox
+                      checked={t.complete}
+                      onChange={() => {
+                        complete(t.id);
+                      }}
+                    />
+                    <Text>{t.title}</Text>
+                  </Flex>
+                ))}
             </Box>
           </Flex>
         </Flex>
