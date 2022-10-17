290fa97aff25adc14d92160f8c13b8c2dfba9efd
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-17 20:26:58 +0100

    pass the note and note content  to a state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import {
+  Box,
   Button,
   Flex,
   Tab,
@@ -9,18 +10,57 @@ import {
   TabPanels,
   Tabs,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import AddNote from "../components/AddNote";
 import NoteCard from "./NoteCard";
+import { MdBookmarkAdd } from "react-icons/md";
 
 const Main = () => {
   const [showAddNewNotePopup, setShowAddNewNotePopup] = useState(false);
   const closePopup = () => {
     setShowAddNewNotePopup(false);
   };
+  const [newNote, setNewNote] = useState([]);
+  const toast = useToast();
+  const showError = (message) => {
+    toast({
+      description: message,
+      status: "error",
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+  const addNewNote = (title, content, date) => {
+    if (!title) {
+      showError("enter title");
+    } else if (!content) {
+      showError("enter note content");
+    } else {
+      toast({
+        description: "Note added successfully",
+        status: "success",
+        duration: 1500,
+        isClosable: true,
+      });
+    }
+
+    const note = {
+      title,
+      content,
+      date,
+    };
+    console.log(note);
+    setNewNote([...newNote, note]);
+  };
+
   return (
     <>
-      <AddNote isOpen={showAddNewNotePopup} closePopup={closePopup} />
+      <AddNote
+        isOpen={showAddNewNotePopup}
+        closePopup={closePopup}
+        addNewNote={addNewNote}
+      />
       <Flex direction={"column"} justifyContent={"center"}>
         <Text fontSize={"20px"} fontWeight={"bold"} mt={"30px"}>
           My Notes
@@ -43,30 +83,38 @@ const Main = () => {
           <TabPanels>
             <TabPanel>
               <Flex gap={"20px"} wrap={"wrap"}>
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
+                {newNote.map((note) => (
+                  <Box
+                    width={["90%", "45%", "25%"]}
+                    bg={"#181819"}
+                    p={"17px"}
+                    borderRadius={"10px"}
+                  >
+                    <Flex>
+                      <Box w={"80%"}>
+                        <Text fontSize={["15px", "20px"]}>{note.title}</Text>
+                        <Text mt={5} fontSize={["10px", "15px"]}>
+                          {note.content}
+                        </Text>
+                      </Box>
+                      <Button>
+                        <Text fontSize={"2xl"}>
+                          <MdBookmarkAdd />
+                        </Text>
+                      </Button>
+                    </Flex>
+                  </Box>
+                ))}
               </Flex>
             </TabPanel>
 
             <TabPanel>
               <Flex gap={"20px"} wrap={"wrap"}>
                 <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
               </Flex>
             </TabPanel>
             <TabPanel>
               <Flex gap={"20px"} wrap={"wrap"}>
-                <NoteCard />
-                <NoteCard />
-                <NoteCard />
                 <NoteCard />
               </Flex>
             </TabPanel>
