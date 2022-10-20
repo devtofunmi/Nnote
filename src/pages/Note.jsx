2682f88940a0d551b54bb03313d4c5a14077bb81
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-20 23:33:53 +0100

    ...

---
+++ src/pages/Note.jsx
@@ -0,0 +1,13 @@
+import { Text } from "@chakra-ui/react";
+import React from "react";
+import DashboardLayout from "../layout/DashboardLayout";
+
+const Note = () => {
+  return (
+    <DashboardLayout>
+      <Text>Hii</Text>
+    </DashboardLayout>
+  );
+};
+
+export default Note;
