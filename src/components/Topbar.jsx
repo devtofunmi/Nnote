8a0711d17d8ee7984aafef50a79b1e7da3bd1f0a
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-28 13:32:21 +0100

    adjust the font size of the welcome

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -7,18 +7,7 @@ const Topbar = () => {
 
   return (
     <Flex justify={"space-between"} px={3} py={2} bg={"#181819"}>
-      <Text>Welcome </Text>
-      {/* <Input
-        w={["70%", "50%"]}
-        bg={"#202225"}
-        placeholder={"Search..."}
-        _placeholder={{
-          color: "#afb1b3",
-        }}
-        onChange={(e) => {
-          searchBar(e.target.value);
-        }}
-      /> */}
+      <Text fontSize={["20px", "30px"]}>Welcome </Text>
 
       <Flex
         align={"center"}
